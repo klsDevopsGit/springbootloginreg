83b2cc36e1d4e951574e57970f04315263dc8698
Lakshman <lkannajoshula@gmail.com>
2023-07-26 12:19:21 +0530

    test folder commited

---
+++ src/test/java/com/net/guides/springboot/loginregistrationspringbootauthjsp/WebappApplicationTests.java
@@ -0,0 +1,12 @@
+package net.guides.springboot.loginregistrationspringbootauthjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
